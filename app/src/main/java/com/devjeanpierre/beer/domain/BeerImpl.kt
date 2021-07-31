45b8a762b9de898a9d981204a2004a2786948b88
JeanpierreSaldivar <saldivarleon5b@gmail.com>
2021-07-31 00:41:41 -0500

    empezando

--- app/src/main/java/com/devjeanpierre/beer/domain/BeerImpl.kt
+++ app/src/main/java/com/devjeanpierre/beer/domain/BeerImpl.kt
@@ -1,8 +1,7 @@
 package com.devjeanpierre.beer.domain
 
-import com.devjeanpierre.beer.data.model.Beer
+
 import com.devjeanpierre.beer.data.model.ResponseDataBeer
-import com.devjeanpierre.beer.data.remote.api.BeerApiClient
 import com.devjeanpierre.beer.data.remote.source.BeerDataSource
 
 class BeerImpl(private val dataSource: BeerDataSource) : BeerRepo {
